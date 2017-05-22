fe5549759ccd9b9f836280992627568cd03b8abf
Brandon Brown <brandon@sudopriest.com>
2017-05-22 00:29:35 -0600

    Added some ingredients shit:

--- app/components/AddNewIngredientField.jsx
+++ app/components/AddNewIngredientField.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-class DocumentInput extends React.Component {
-  render() {
-    return <input 
-      type="file" 
-      name={ `document-${ this.props.index }-document` } 
-    />;
-  }
-}
-export class DocumentsFieldSet extends React.Component{
-  constructor(props){
+class DocumentInput extends React.Component ::
+  render() ::
+    return @
+        <input 
+            type="file" 
+            name={ `document-${ this.props.index }-document` } 
+        />
+
+
+export class DocumentsFieldSet extends React.Component ::
+
+  constructor(props) ::
     super(props);
 
     this.state = { 
@@ -17,25 +19,23 @@ export class DocumentsFieldSet extends React.Component{
     }
     
     this.add = this.add.bind(this);
-  }
+ 
 
-  add() {
+  add() ::
     const documents = this.state.documents.concat(DocumentInput);
     this.setState({ documents });
-  }
-  
-  render () {
-    const documents = this.state.documents.map((Element, index) => {
-      return <Element key={ index } index={ index } />
-    });
 
-    return <div>
-      <button onClick={ this.add }>Add</button>
-    
-      <div className="inputs">
-        { documents }
-      </div>
-    </div>
-  }
-}
+  
+  render () ::
+    const mkEl = (Element, index) => (<Element key={ index } index={ index } />)
+    const documents = this.state.documents.map @ mkEl
+
+    return @
+        <div>
+          <button onClick={ this.add }>Add</button>
+          
+          <div className="inputs">
+              { documents }
+          </div>
+        </div>
 
